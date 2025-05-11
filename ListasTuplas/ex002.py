29049e0ecbbcd6e67d331b818f2d7b0db517700f
Roger Reis <roger.gdreis@gmail.com>
2025-05-11 16:57:50 -0300

    exficcao002

---
+++ ListasTuplas/ex002.py
@@ -0,0 +1,16 @@
+# Faça um Programa que leia um vetor de 10 números reais e mostre-os na ordem inversa.
+
+vetor = []
+print("Digite 5 números inteiros:")
+for i in range(10):
+    while True:
+        try:
+            num = float(input(f"Digite o número {i+1}: "))
+            vetor.append(num)
+            break
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número inteiro.")
+
+print("Os números digitados foram:")
+for num in reversed(vetor):
+    print(num)
